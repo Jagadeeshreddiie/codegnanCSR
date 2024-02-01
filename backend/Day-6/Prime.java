de6d372567541d7687306c8148260e4ca4c09c28
Jagadeeshreddiie <jagadeeshreddii4u@gmail.com>
2024-02-01 15:56:53 +0530

    prime updated

--- backend/Day-6/Prime.java
+++ backend/Day-6/Prime.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 
-public class Prime{
-    public static void main(String [] ar){
-        Scanner sc=new Scanner(System.in);
+public class Prime {
+    public static void main(String[] ar) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number to check : ");
-        int i=sc.nextInt();
+        int i = sc.nextInt();
         sc.close();
-        if(i==0 || i==1){
+        if (i == 0 || i == 1) {
             System.out.println("Neither prime nor composite");
             return;
         }
-        int count=0;
-        for(int j=2;j<i;j++){
-            if(i%j==0){
-                count++;
-                break;
+        
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                System.out.println("Not a Prime");
+                return;
             }
         }
-        if(count>0){
-            System.out.println("Not a prime");
-        }
-        else{
-            System.out.println("Prime");
-        }
+
+        System.out.println("prime");
+
     }
 }
